162ce6e46aadd858c2111de07fc6194f39c5d08f
hjin302 <enro302@naver.com>
2020-03-17 15:46:27 +0900

    비밀번호에 따른 토스트 띄우기

--- app/src/main/java/com/example/a20200317_02_java_first/MainActivity.java
+++ app/src/main/java/com/example/a20200317_02_java_first/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.a20200317_02_java_first.databinding.ActivityMainBinding;
 
@@ -31,8 +32,23 @@ public class MainActivity extends AppCompatActivity {
 
                 String input = binding.inputTxt.getText().toString();
 
+                Log.d("입력값 확인", input);
+
                 binding.appearTxt.setText(input);
                 binding.inputTxt.setText("");
+
+                binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String pw = binding.pwTxt.getText().toString();
+
+                        if(pw.equals("1234")) {
+                            Toast.makeText(MainActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(MainActivity.this, "잘못된 비번입니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
