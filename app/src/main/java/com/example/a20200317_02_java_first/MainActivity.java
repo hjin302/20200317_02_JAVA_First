8b5a083a440fd0690a196c5faba030ecbfdf3929
hjin302 <enro302@naver.com>
2020-03-17 15:03:18 +0900

    데이터 바인딩 세팅

--- app/src/main/java/com/example/a20200317_02_java_first/MainActivity.java
+++ app/src/main/java/com/example/a20200317_02_java_first/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.a20200317_02_java_first;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.a20200317_02_java_first.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding = null;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+
     }
 }
